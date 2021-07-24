b69050ac1448e2ab9a2c59bc5da126d3b848e82c
Snehil Shubham <snehil1693@gmail.com>
2021-07-24 16:56:33 -0700

    nginx install

--- var.sh
+++ var.sh
@@ -1,7 +1,16 @@
-!/usr/bin/bash
+#!/usr/bin/bash
 
 a=10
 b=20
 ADD=$((a+b))
 # shellcheck disable=SC1036
-echo ADD of 2 numbers $ADD
+echo "ADD of 2 numbers" $ADD
+
+
+b=30
+c=80
+MULTIPLY=$((a*b))
+
+echo "Multiply 2 numbers" $MULTIPLY
+
+
